69757096e1872bd85dbc262cb9eb28fc2e1746b1
Grégoire <greg.joncour@gmail.com>
2017-11-03 02:32:06 +0100

    cors

--- app.js
+++ app.js
@@ -26,6 +26,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'x-access-token,Authorization,Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 app.use('/', index);
 app.use('/users', users);
 app.use('/accounts', accounts);
--- routes/accounts.js
+++ routes/accounts.js
@@ -4,10 +4,7 @@ var Users = require('../controllers/Accounts');
 var Jwt = require('jsonwebtoken');
 
 var tokenAuth = function(req, res, next){
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-    
+
     var token = req.body.token || req.query.token || req.headers['x-access-token'] || req.headers['Authorization'] || req.get('authorization').split('Bearer ')[1];
     
     if (token) {
